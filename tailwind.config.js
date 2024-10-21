6bb54ea1edd262513a37dd2d8abf260f3d8a4d63
Mostafa <Mostafa00731@gmail.com>
2024-10-21 10:05:04 +0200

    Mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './*.html',  // Alla HTML-filer i rotmappen
-    './src/**/*.html',  // Alla HTML-filer i /src eller dess undermappar (om du använder en src-mapp)
-    './src/**/*.js',  // Alla JavaScript-filer om du använder JS med Tailwind-klasser
+    "*.html",  // Alla HTML-filer i rotmappen
   ],
   theme: {
     extend: {},
